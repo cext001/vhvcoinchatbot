cc952d2916ac5118950967a863099e0f487abc7d
unknown <cext001.hexaware@gmail.com>
2018-04-19 22:37:46 +0530

    added bot framework code

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3978, function () {
+server.use(restify.CORS());
+server.use(restify.queryParser());
+server.listen(process.env.port || process.env.PORT || 4000, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-
-// Create chat connector for communicating with the Bot Framework Service
+// Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.APP_ID,
     appPassword: process.env.APP_SECRET
 });
 
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
+var bot = new builder.UniversalBot(connector);
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+server.post('/', connector.listen());
+
+server.get('/', function (req, res) {
+    if (req.query.hub.verify_token === _config.ms.messengerSecret) {
+        res.write(req.query.hub.challenge);
+        res.end();
+    } else {
+        res.send('Error, wrong validation token');
+    }
 });

a8a2c6d4b0797d1ce4c854aa4f0e3bdeecadf935
cext001 <cext001.hexaware@gmail.com>
2018-04-20 09:35:47 +0530

    added sim ple code

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 'use strict';
 
-const apiai = require('apiai');
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const REST_PORT = process.env.PORT || 5000;
 
-const SkypeBot = require('./skypebot');
-const SkypeBotConfig = require('./skypebotconfig');
-
-const REST_PORT = (process.env.PORT || 5000);
-
-const botConfig = new SkypeBotConfig(
-    "b37323d3c6dd43eeb037567483e10160",
-    "en",
-    "f6f66a29-f184-48a0-9913-bda96c22be4f",
-    "bkVCGV20*%+foicfITP333@"
-);
-
-const skypeBot = new SkypeBot(botConfig);
-
-// console timestamps
-require('console-stamp')(console, 'yyyy.mm.dd HH:MM:ss.l');
-
-const app = express();
-app.use(bodyParser.json());
-
-app.post('/chat', skypeBot.botService.listen());
+app.post('/chat', (req, res) => {
+    if (req.body.queryResult.action = "claim.raiseissue") {
+        res.json({"messages": [
+            {
+              "speech": "Text response",
+              "type": 0
+            }
+          ]});
+    }
+   // res.send('Hello World!');
+})
 
 app.listen(REST_PORT, function () {
     console.log('Rest service ready on port ' + REST_PORT);

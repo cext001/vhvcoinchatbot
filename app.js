85672dbc373d300520c0819e7498e48d5cc488a0
cext001 <cext001.hexaware@gmail.com>
2018-04-20 15:23:18 +0530

    added new intents

--- app.js
+++ app.js
@@ -12,16 +12,32 @@ const REST_PORT = process.env.PORT || 8080;
 app.post('/api/messages', (req, res) => {
     console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
     if (req.body.result) {
-        if (req.body.result.action == "claimraiseissue.claimraiseissue-custom") {
-            console.log("inside: claimraiseissue.claimraiseissue-custom");
-            if(req.body.result.resolvedQuery == "Yes Please") {
-                console.log("inside: claimraiseissue.claimraiseissue-custom user selected yes");
+        switch (req.body.result.action) {
+            case "claimraiseissue.claimraiseissue-custom":
+                console.log("inside: claimraiseissue.claimraiseissue-custom");
+                if (req.body.result.resolvedQuery == "Yes Please") {
+                    console.log("inside: claimraiseissue.claimraiseissue-custom user selected yes");
+                    res.json({
+                        speech: "Please can you tell me your policy number",
+                        displayText: "Please can you tell me your policy number",
+                        type: 0
+                    }).end();
+                }
+                break;
+            case "claim.getdateandtime":
                 res.json({
-                    speech: "Please can you tell me your policy number",
-                    displayText: "Please can you tell me your policy number",
-                    source: ''
+                    messages: [{
+                        speech: "Please can you tell me your policy number",
+                        displayText: "Please can you tell me your policy number",
+                        type: 0
+                    }, {
+                        speech: "Please can you tell me your policy number",
+                        displayText: "Please can you tell me your policy number",
+                        type: 0
+                    },]
                 }).end();
-            }
+
+                break;
         }
     }
 })

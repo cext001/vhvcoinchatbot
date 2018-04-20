3e253a6fe711f77c188415a5752ad4f77db2b51b
cext001 <cext001.hexaware@gmail.com>
2018-04-20 09:57:04 +0530

    added legacy code

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 'use strict';
 
-const express = require('express')
-const app = express()
-const REST_PORT = process.env.PORT || 5000;
+const apiai = require('apiai');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.post('/chat', (req, res) => {
-    console.log(req.body);
-    if (req.body.queryResult.action = "claim.raiseissue") {
-        res.json({"messages": [
-            {
-              "speech": "Text response",
-              "type": 0
-            }
-          ]});
-    }
-   // res.send('Hello World!');
-})
+const SkypeBot = require('./skypebot');
+const SkypeBotConfig = require('./skypebotconfig');
+
+const REST_PORT = (process.env.PORT || 5000);
+
+const botConfig = new SkypeBotConfig(
+    "b37323d3c6dd43eeb037567483e10160",
+    "en",
+    "f6f66a29-f184-48a0-9913-bda96c22be4f",
+    "bkVCGV20*%+foicfITP333@"
+);
+
+const skypeBot = new SkypeBot(botConfig);
+
+// console timestamps
+require('console-stamp')(console, 'yyyy.mm.dd HH:MM:ss.l');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/chat', skypeBot.botService.listen());
 
 app.listen(REST_PORT, function () {
     console.log('Rest service ready on port ' + REST_PORT);

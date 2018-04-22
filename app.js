b6d0eb13e7ae492ef5b944ba253f336a44e50533
cext001 <cext001.hexaware@gmail.com>
2018-04-22 19:12:57 +0530

    added new logic

--- app.js
+++ app.js
@@ -211,15 +211,11 @@ app.post('/api/messages', (req, res) => {
                 console.log("inside claim.getdamegedparts");
                 console.log(req.body.result.contexts[0]);
                 if (req.body.result.resolvedQuery == "Finish") {
-                    console.log("inside: claimgetcauseofdamage.claimgetcauseofdamage-no");
+                    console.log("inside: claimgetcauseofdamage.claimgetcauseofdamage-no Finish");                    
                     res.json({
-                        messages: [
-                            {
-                                platform: "skype",
-                                speech: "Thanks you have selected " + verchiclepartslist,
-                                type: 0
-                            }
-                        ]
+                        speech: "You have selected " + verchiclepartslist,
+                        displayText: "You have selected " + verchiclepartslist,
+                        type: 0
                     }).end();
                 } else {
                     var verchiclepartsincontext = req.body.result.contexts[0].parameters.partsofvehicle;

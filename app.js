15aa87bbc30289e31e832bd2b3709d80c512fe8a
cext001 <cext001.hexaware@gmail.com>
2018-04-23 09:03:59 +0530

    added correction in foplloeup[ intent

--- app.js
+++ app.js
@@ -13,17 +13,6 @@ app.post('/api/messages', (req, res) => {
     console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
     if (req.body.result) {
         switch (req.body.result.action) {
-            case "claimraiseissue.claimraiseissue-yes":
-                console.log("inside: claimraiseissue.claimraiseissue-custom");
-                if (req.body.result.resolvedQuery == "Yes Please") {
-                    console.log("inside: claimraiseissue.claimraiseissue-yes user selected yes");
-                    res.json({
-                        speech: "Please can you tell me your policy number",
-                        displayText: "Please can you tell me your policy number",
-                        type: 0
-                    }).end();
-                }
-                break;
             case "claim.getdateandtime":
                 console.log("inside: claim.getdateandtime");
                 res.json({

76d0b506415be79ead73a283e269905cd6becc30
cext001 <cext001.hexaware@gmail.com>
2018-04-25 11:45:48 +0530

    added contexts

--- helper.js
+++ helper.js
@@ -1,5 +1,6 @@
 var request = require("request");
 var config = require('./config');
+var lodash = require('lodash');
 
 module.exports = {
     "getPolicyTypes": function () {
@@ -24,8 +25,15 @@ module.exports = {
                     console.log('response', response);
                     reject(error);
                 } else {
-                    console.log(body);
-                    resolve(body);
+                    var lossTypes = [];
+                    lodash.forEach(body.result, function (value, key) {
+                        lossTypes.push({
+                            postback: value.code,
+                            text: value.name
+                        });
+                    });
+                    console.log('losstypes',lossTypes);
+                    resolve(lossTypes);
                 }
             });
         });
@@ -57,10 +65,38 @@ module.exports = {
                     console.log('response', response);
                     reject(error);
                 } else {
-                    console.log('body',body);
+                    console.log('body', body);
                     resolve(body.result);
                 }
             });
         });
-    }
+    },
+    "getLossType": function () {
+        return new Promise(function (resolve, reject) {
+            console.log('getLossType');
+            var options = {
+                method: 'POST',
+                url: config.base_url + 'cc/service/edge/hexaware/common',
+                headers:
+                    {
+                        'cache-control': 'no-cache',
+                        'content-type': 'application/json',
+                        authorization: 'Basic c3U6Z3c='
+                    },
+                body: { jsonrpc: '2.0', method: 'getLossType', params: [] },
+                json: true
+            };
+            console.log(JSON.stringify(options));
+            request(JSON.parse(JSON.stringify(options)), function (error, response, body) {
+                if (error) {
+                    console.log('error', error);
+                    console.log('response', response);
+                    reject(error);
+                } else {
+                    console.log(body);
+                    resolve(body);
+                }
+            });
+        });
+    },
 };

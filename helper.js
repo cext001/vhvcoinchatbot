697c27acac6365fd1372e6596a45ee85c8a6c5d9
cext001 <cext001.hexaware@gmail.com>
2018-05-07 12:20:41 +0530

    added new changes

--- helper.js
+++ helper.js
@@ -3,41 +3,6 @@ var config = require('./config');
 var lodash = require('lodash');
 
 module.exports = {
-    "getPolicyTypes": function () {
-        return new Promise(function (resolve, reject) {
-            console.log('getPolicyTypes');
-            var options = {
-                method: 'POST',
-                url: config.base_url + 'cc/service/edge/hexaware/common',
-                headers:
-                    {
-                        'cache-control': 'no-cache',
-                        'content-type': 'application/json',
-                        authorization: 'Basic c3U6Z3c='
-                    },
-                body: { jsonrpc: '2.0', method: 'getPolicyTypes', params: [] },
-                json: true
-            }
-            console.log(JSON.stringify(options));
-            request(JSON.parse(JSON.stringify(options)), function (error, response, body) {
-                if (error) {
-                    console.log('error', error);
-                    console.log('response', response);
-                    reject(error);
-                } else {
-                    var policyTypes = [];
-                    lodash.forEach(body.result, function (value, key) {
-                        policyTypes.push({
-                            postback: value.code,
-                            text: value.name
-                        });
-                    });
-                    console.log('from helper getpolicytypes', JSON.stringify(policyTypes));
-                    resolve(policyType);
-                }
-            });
-        });
-    },
     "searchPolicies": function (policyNumber) {
         return new Promise(function (resolve, reject) {
             console.log('getClaimPaymentDetails');
